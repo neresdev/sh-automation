0bf74905da24767a2bb0c120e1cc8caf336f2e0d
neresdev <neredev@gmail.com>
2022-07-06 20:57:31 -0300

    Script gerando .zip com versão vindo por parâmetro

--- first.sh
+++ first.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "first script"
+# recebe a versão por parametro na hora da execução
+VERSAO_API=$1
+function zipaJar(){
+	echo "gerando versao $VERSAO_API zip do projeto";
+	rm -rf /home/neres/visualmix/jars/mercafacil/*;
+	cp ~/visualmix/develop/wks/wk-mercafacil/vm-mercafacil/target/vm-mercafacil-api.jar ~/visualmix/jars/mercafacil;
+	cd ~/visualmix/jars/mercafacil;
+	zip vm-mercafacil-api-$VERSAO_API vm-mercafacil-api.jar;
+
+}
+zipaJar;
