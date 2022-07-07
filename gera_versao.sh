f7e82360d6479d037504fa6ebf936addc876ea1f
neresdev <neredev@gmail.com>
2022-07-06 21:19:15 -0300

    adicionando o caminho do jar junto com o nome do jar como parâmetro

--- gera_versao.sh
+++ gera_versao.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
 # recebe a versão por parametro na hora da execução
 VERSAO_API=$1
 
+# recebe o caminho do .jar junto com o nome do .jar por parâmetro na hora da execução
+CAMINHO_JAR=$2
+
 function zipaJar(){
-	echo "gerando versao $VERSAO_API zip do projeto";
+	echo "gerando o .zip da versao $VERSAO_API do projeto";
 	cd ~/visualmix/jars/mercafacil;
 	zip vm-mercafacil-api-$VERSAO_API.zip vm-mercafacil-api.jar;
 
 }
 
 function copiaJar(){
-	cp ~/visualmix/develop/wks/wk-mercafacil/vm-mercafacil/target/vm-mercafacil-api.jar ~/visualmix/jars/mercafacil;
+	cp $CAMINHO_JAR ~/visualmix/jars/mercafacil;
 }
 
 function limpaPasta(){
